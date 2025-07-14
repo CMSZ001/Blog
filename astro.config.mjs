32da58f8472f8799eccc1cbf1ef36922be399acf
CMSZ001 <130162476+CMSZ001@users.noreply.github.com>
2025-07-14 07:56:17 +0000

    install astro-expressive-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,7 +40,7 @@ export default defineConfig({
         destination: 'https://github.cmsz.dpdns.org/',
     }
     },
-    output: 'static',
+    output: 'server',
 	adapter: netlify(),
 	prefetch: true,
 	integrations: [
