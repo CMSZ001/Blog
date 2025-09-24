cb53cb5d500bde407df329052a4937a1ca2458fc
CMSZ <me@acmsz.top>
2025-09-24 11:30:43 +0800

    修改了头像

--- src/config.ts
+++ src/config.ts
@@ -66,7 +66,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "https://q2.qlogo.cn/headimg_dl?dst_uin=2706210065&spec=0", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "CMSZ",
 	bio: "世界美好与莫环环相扣。",
 	links: [
